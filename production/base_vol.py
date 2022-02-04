5442e016d86895a95e4b5728abd14b0c770f8c9d
Patrick Guo <guopatrick.courses@gmail.com>
2022-02-04 11:43:53 -0800

    garch and gjr garch tuning into function

--- production/base_vol.py
+++ production/base_vol.py
@@ -7,11 +7,8 @@ n = int(len(returns)*0.4)
 split_date = returns[-n:].index
 
 # GARCH
-garch = arch_model(returns, mean='zero', vol='GARCH', p=1, o=0, q=1).fit(disp='off')
-bic_garch = []
-
-
-def best_garch():
+def best_GARCH():
+    bic_garch = []
     for p in range(1, 5):
         for q in range(1, 5):
             garch = arch_model(returns, mean='zero', vol='GARCH', p=p, o=0, q=q).fit(disp='off')
@@ -19,7 +16,7 @@ def best_garch():
             if garch.bic == np.min(bic_garch):
                 best_param = p, q
     garch = arch_model(returns, mean='zero', vol='GARCH',
-                    p = best_param[0], o = 0, q = best_param[1]).fit(disp='off')
+                    p=best_param[0], o=0, q=best_param[1]).fit(disp='off')
     return garch
-forecast = best_garch().forecast(start=split_date[0])
-forecast_garch = forecast
+garch = best_GARCH()
+forecast_garch = garch.forecast(start=split_date[0])
