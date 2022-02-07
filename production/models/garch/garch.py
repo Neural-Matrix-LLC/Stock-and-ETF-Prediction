737d62877947afa3cb3cfc71743ff65592cee97f
Patrick Guo <guopatrick.courses@gmail.com>
2022-02-06 21:36:57 -0800

    gjrgarch.py

--- production/models/garch/garch.py
+++ production/models/garch/garch.py
@@ -3,11 +3,10 @@ import pandas as pd
 from arch import arch_model
 import logging
 
-
 # GARCH
-def garch(P=1, Q=1, tune=True):
+def garch(returns, P=1, Q=1, tune=True):
     if tune:
-        logging.info(f'{name} Tune GARCH.')
+        logging.info(f'Tune GARCH.')
         bic_garch = []
         for p in range(1, 5):
             for q in range(1, 5):
@@ -16,9 +15,9 @@ def garch(P=1, Q=1, tune=True):
                 if garch.bic == np.min(bic_garch):
                     best_param = p, q
         garch = arch_model(returns, mean='zero', vol='GARCH', p=best_param[0], o=0, q=best_param[1]).fit(disp='off')
-        logging.info(f'{name} GARCH({best_param[0]}, {best_param[0]}).')
+        logging.info(f'GARCH({best_param[0]}, {best_param[0]}).')
         return garch
-    
+   
     garch = arch_model(returns, mean='zero', vol='GARCH', p=P, o=0, q=Q).fit(disp='off')
-    logging.info(f'{name} GARCH({P}, {Q}).')
+    logging.info(f'GARCH({P}, {Q}).')
     return garch
