3e070e4e41b51a47407eff0a9afd514c44945f4c
Patrick Guo <guopatrick.courses@gmail.com>
2022-02-07 10:05:33 -0800

    import modules on main

--- production/.ipynb_checkpoints/main-checkpoint.py
+++ production/.ipynb_checkpoints/main-checkpoint.py
@@ -1,5 +1,6 @@
 import logging
 from data import fetch_data, processing_lstm
+from model import garch, gjrgarch, egarch, svr_linear, svr_rbf, NN_vol, DL_vol, lstm
 
 logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 host="143.244.188.157"
@@ -11,9 +12,6 @@ password="Pat#21$rick"
 def main():
     logging.info(f'Start main.py')
     fetch_data.get_df()
-    
-    
-    
     
     
 
