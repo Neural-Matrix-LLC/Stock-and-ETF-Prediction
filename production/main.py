d3f82a562bef4ad67b88c88b5903ec921f4187a6
Patrick Guo <guopatrick.courses@gmail.com>
2022-02-25 16:21:58 -0800

    main try excepts and json loaders

--- production/main.py
+++ production/main.py
@@ -1,9 +1,9 @@
-import numpy as np
 import pandas as pd
 import logging
+from os import path
 from datetime import date
 from data import data, processing
-from model import garch, svr, mlp, DL_vol, lstm
+from model import garch, svr, mlp, lstm
 
 logging.basicConfig(filename='logging/app.log', filemode='w', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
@@ -13,37 +13,49 @@ user="patrick-finProj"
 password="Pat#21$rick"
 
 def garch_predict(symbol, returns):
-    dpath = f"model/params/garch/{symbol}.csv"
-    if path.isfile(dpath):
-        params = load_csv(dpath)
-        garch_predict = garch.predict(returns, params)
-        return garch_predict
-    else:
-        params = garch.tune(symbol, returns)
-        garch_predict = garch.predict(returns, params)
-        return garch_predict
+    try:
+        dpath = f"model/params/garch/{symbol}.csv"
+        if path.isfile(dpath):
+            logging.info(f'Load params from {dpath}')
+            params = pd.read_json(dpath)
+            garch_predict = garch.predict(returns, params)
+            return garch_predict
+        else:
+            params = garch.tune(symbol, returns)
+            garch_predict = garch.predict(returns, params)
+            return garch_predict
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
 
 def svr_predict(symbol, X, realized_vol):
-    dpath = f"model/params/svr/{symbol}.csv"
-    if path.isfile(dpath):
-        params = load_csv(dpath)
-        svr_predict = svr.predict(X, realized_vol, params)
-        return svr_predict
-    else:
-        params = svr.tune(X, realized_vol)
-        svr_predict = svr.predict(symbol, X, realized_vol, params)
-        return svr_predict
+    try:
+        dpath = f"model/params/svr/{symbol}.csv"
+        if path.isfile(dpath):
+            logging.info(f'Load params from {dpath}')
+            params = pd.read_json(dpath)
+            svr_predict = svr.predict(X, realized_vol, params)
+            return svr_predict
+        else:
+            params = svr.tune(X, realized_vol)
+            svr_predict = svr.predict(symbol, X, realized_vol, params)
+            return svr_predict
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
 
 def mlp_predict(symbol, X, realized_vol):
-    dpath = f"model/params/mlp/{symbol}.csv"
-    if path.isfile(dpath):
-        params = load_csv(dpath)
-        mlp_predict = mlp.predict(X, realized_vol, params)
-        return mlp_predict
-    else:
-        params = mlp.tune(X, realized_vol)
-        mlp_predict = mlp.predict(symbol, X, realized_vol, params)
-        return mlp_predict
+    try:
+        dpath = f"model/params/mlp/{symbol}.csv"
+        if path.isfile(dpath):
+            logging.info(f'Load params from {dpath}')
+            params = pd.read_json(dpath)
+            mlp_predict = mlp.predict(X, realized_vol, params)
+            return mlp_predict
+        else:
+            params = mlp.tune(X, realized_vol)
+            mlp_predict = mlp.predict(symbol, X, realized_vol, params)
+            return mlp_predict
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
 
 def main():
     logging.info(f'Start main.py')
