2724ba078dfd58c8cf9fe3b042989afa491fe307
Patrick Guo <guopatrick.courses@gmail.com>
2022-03-04 13:14:00 -0800

    Update garch.py

--- production/model/garch.py
+++ production/model/garch.py
@@ -1,5 +1,3 @@
-import numpy as np 
-import pandas as pd
 from arch import arch_model
 import logging
 from multiprocessing import Pool
@@ -24,7 +22,7 @@ def evaluate_model(residuals, lags=50):
         results['LM_pvalue'] = [arch_test[1], arch_test[1] < .05]
         return results
     except Exception as e:
-        logging.error("Exception occurred", exc_info=True)
+        logging.error("evaluate_model: Exception occurred")
 
 def p_calc_model(data, mean, vol, p, q, o, dist):
     res = {}
@@ -45,7 +43,7 @@ def p_calc_model(data, mean, vol, p, q, o, dist):
         res['params']['dist'] = dist
         return res
     except Exception as e:
-        logging.error("Exception occurred", exc_info=True)
+        logging.error("p_calc_model: Exception occurred: {}".format(e))
 
 def multip_gridsearch(data, mean_list, vol_list, p_rng, q_rng, o_rng, dist_list, num_p=10):
     n_sym = len(p_rng) * len(q_rng)
@@ -62,7 +60,6 @@ def multip_gridsearch(data, mean_list, vol_list, p_rng, q_rng, o_rng, dist_list,
                             for dist in dist_list:
                                 ll.append((data, mean, vol, p, o, q, dist))  
         logging.info(f"Starting {n_sym} threads")
-        logging.info(ll)
         with Pool(processes=num_p) as pool:
             all_results = pool.starmap(p_calc_model, ll)
         logging.info("All Grid threads Finish.")
@@ -75,7 +72,7 @@ def multip_gridsearch(data, mean_list, vol_list, p_rng, q_rng, o_rng, dist_list,
             elif results['LM_pvalue'][1] is False:
                 top_models.append(results)
     except Exception as e:
-        logging.error("Exception occurred", exc_info=True)
+        logging.error("multip_gridsearch: Exception occurred", exc_info=True)
     top_models.append(top_results)
     return top_models
 
@@ -84,9 +81,9 @@ def tune(symbol, data):
     num_p = 10
     mean_list = ['Constant', 'Zero', 'LS', 'AR', 'ARX', 'HARX']
     vol_list = ['GARCH', 'ARCH', 'EGARCH', 'HARCH'] 
-    p_rng = range(1,20)
-    o_rng = range(0,20)
-    q_rng = range(0,20)
+    p_rng = range(1,3)
+    o_rng = range(0,3)
+    q_rng = range(0,3)    
     dist_list = ['normal', 't', 'skewt', 'ged']
     try:
         logging.info("Start GARCH Process")
@@ -102,13 +99,9 @@ def tune(symbol, data):
         top_params = {"mean": mean, "vol": vol, "p": p, "o": o, "q": q, "dist": dist}
         logging.info(f'Best GARCH params={top_params}')
 
-        with open(f"params/garch/{symbol}", "w") as outfile:
-            json.dump(top_params, outfile)
-        logging.info(f'Export best SVR parameters to JSON')
-
         return top_params
     except Exception as e:
-        logging.error("Exception occurred", exc_info=True)
+        logging.error("tune: Exception occurred", exc_info=True)
 
 def predict(data, params):
     try:
@@ -118,4 +111,4 @@ def predict(data, params):
         pred = model_fit.forecast(horizon=1)
         return pred.variance.values[-1,:][0] / 100  # Return the variance
     except Exception as e:
-        print("calc_model:{}".format(e)) 
+        logging.error("predict: Exception occurred")
