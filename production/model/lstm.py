bc962d383ae114add5d2829dfde9fe97bd6fac28
Patrick Guo <guopatrick.courses@gmail.com>
2022-03-11 15:58:49 -0800

    Update lstm.py

--- production/model/lstm.py
+++ production/model/lstm.py
@@ -8,6 +8,7 @@ import logging
 
 # normalize data
 def normalize(close):
+    logging.info(f'LSTM normalize')
     try:
         scaler = MinMaxScaler(feature_range = (0,1))
         scaled_data = scaler.fit_transform(close.values.reshape(-1, 1))
@@ -17,6 +18,7 @@ def normalize(close):
         
 # test-train split
 def create_dataset(dataset, time_step):
+    logging.info(f'LSTM create_dataset')
     try:
         x_data, y_data = [], []
         for i in range(len(dataset)-time_step-1):
@@ -28,12 +30,17 @@ def create_dataset(dataset, time_step):
 
 # Preparing train and test data
 def test_train_split(scaled_data, train_size, time_step):
+    logging.info('LSTM test_train_split({},{},{})'.format(scaled_data.shape,train_size,time_step))
     try:
         training_size = int(len(scaled_data) * train_size)
         test_size = len(scaled_data) - training_size
+        test_size = test_size if test_size > 105 else 105
+        training_size = len(scaled_data) - test_size
+        logging.debug("-> training_size:{}\ttest_size:{}".format(training_size, test_size))
         train_data, test_data = scaled_data[0:training_size, :], scaled_data[training_size:len(scaled_data), :1]
         X_train, y_train = create_dataset(train_data, time_step)
         X_test, y_test = create_dataset(test_data, time_step)
+        logging.debug("x_train:{}, y_train:{}, X_test:{}, y_test:{}".format(X_train.shape,y_train.shape,X_test.shape,y_test.shape))
         X_train = X_train.reshape(X_train.shape[0], X_train.shape[1] , 1)
         X_test = X_test.reshape(X_test.shape[0], X_test.shape[1] , 1)
         return X_train, y_train, X_test, y_test
@@ -42,6 +49,7 @@ def test_train_split(scaled_data, train_size, time_step):
 
 # Compile Model
 def build_model(hp):
+    logging.info(f'LSTM build_mode')
     try:
         model = Sequential()
         model.add(layers.LSTM(units = hp.Choice('layer1_units', [10,20,30,40,50,60,70,80,90,100]), return_sequences=True,input_shape=(100,1)))
@@ -58,7 +66,8 @@ def build_model(hp):
         logging.error("Exception occurred", exc_info=True)
 
 # Tune Model Parameters
-def k_tuner(X_train, y_train):
+def k_tuner(symbol, X_train, y_train, X_test, ytest):
+    logging.info(f'LSTM k_tuner')
     try:
         tuner = keras_tuner.RandomSearch(
             build_model,
@@ -75,17 +84,19 @@ def k_tuner(X_train, y_train):
         logging.error("Exception occurred", exc_info=True)
 
 # LSTM Tune
-def tune(close):
+def tune(symbol, close):
+    logging.info(f'Tune LSTM')
     try:
         scaler, scaled_data = normalize(close)
         X_train, y_train, X_test, y_test = test_train_split(scaled_data, train_size=0.8, time_step=100)
-        model = k_tuner(X_train, y_train)
+        model = k_tuner(symbol, X_train, y_train, X_test, y_test)
         return model
     except Exception as e:
         logging.error("Exception occurred", exc_info=True)
 
 # LSTM prediction    
 def predict(model, close):
+    logging.info(f'LSTM Predict')
     try:
         scaler, scaled_data = normalize(close)
         X_train, y_train, X_test, y_test = test_train_split(scaled_data, train_size=0.9, time_step=100)
