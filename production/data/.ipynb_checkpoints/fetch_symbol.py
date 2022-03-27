6e5461860bc58cfc58fc1c79a4b96807cbe31fc5
Patrick Guo <guopatrick.courses@gmail.com>
2022-03-27 15:16:15 -0700

    remove checkpoints

--- production/data/.ipynb_checkpoints/fetch_symbol.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-stock_path = "stocks_and_etfs/stocks.csv"
-etf_path = "stocks_and_etfs/etf.csv"
-
-with open('some.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
